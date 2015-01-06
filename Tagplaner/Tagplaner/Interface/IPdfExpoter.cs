b26171dbb1de047a96777d0ece6ea477cda48816
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-06 14:04:56 +0000

    - Initialversion, erster Parameter für Informationen über Kalendertage muss noch definiert werden

---
+++ Tagplaner/Tagplaner/Interface/IPdfExpoter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tagplaner.Interface
+{
+    interface IPdfExporter
+    {
+        // erster Parameter muss noch definiert werden
+        bool exportPdf(Object[] objects, string filename);
+    }
+}
