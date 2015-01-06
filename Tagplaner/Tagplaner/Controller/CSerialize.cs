e297290d55d1a4aab2577691f014f7812ebe9423
holler.christopher <holler.christopher@googlemail.com>
2015-01-06 13:25:17 +0000

    Interface und Controller für Klasse Serialize erstellt

---
+++ Tagplaner/Tagplaner/Controller/CSerialize.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace Tagplaner
+{
+    class CSerialize : ISerialize
+    {
+        private static BinaryFormatter formatter;
+        private static FileStream stream;
+
+        public static void SerializeObject(Object obj)
+        {
+            stream = new FileStream(@"C:\MyObjects.dat", FileMode.Create);
+            formatter.Serialize(stream,obj);
+            stream.Close();
+        }
+        public static Object DeserializeObjetct(Object obj)
+        {
+            FileStream stream = new FileStream(@"C:\MyObject.dat", FileMode.Open);
+            return formatter.Deserialize(stream);
+        }
+    }
+}
---
+++ Tagplaner/Tagplaner/Interface/ISerialize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tagplaner
+{
+    class ISerialize
+    {
+        static void SerializeObject(Object obj);
+        static void DeserializeObject(Object obj);
+    }
+}
