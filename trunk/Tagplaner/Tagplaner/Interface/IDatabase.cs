f560971fdb8339d78cbb9d25d1c1eebe7738de49
lf2freek@gmail.com <lf2freek@gmail.com@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-06 14:13:31 +0000

    erste version des Interfaces

---
+++ trunk/Tagplaner/Tagplaner/Interface/IDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Tagplaner.Interface
+{
+    interface IDatabase
+    {
+        bool ConnectDatabase();
+        bool CloseDatabase();
+
+        //Könnte man in Select, Update und delete aufteilen
+        SQLiteDataReader Executequery(string query);
+
+
+    }
+}
