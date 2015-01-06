df33bb92ffa1d131e4ad8aefadff07cf946fa1cf
F.Smudo@gmail.com <F.Smudo@gmail.com@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-06 15:41:22 +0000

    Funktion zum Lesen der CSV ausformuliert

--- trunk/Tagplaner/Tagplaner/Controller/CHoliday.cs
+++ trunk/Tagplaner/Tagplaner/Controller/CHoliday.cs
@@ -10,34 +10,44 @@ namespace Tagplaner
 {
     class CHoliday : IHoliday
     {
-        public List<MHoliday> GetHolidayDummy(String region)
+       /* public List<MHoliday> GetHoliday(String region)
         {
             List<MHoliday> holidayArray = new List<MHoliday>();
             MHoliday mHoliday = new MHoliday(new DateTime(2012, 12, 25), "1. Weihnachtstag");
             holidayArray.Add(mHoliday);
             return holidayArray;
-        }
+        } */
 
+        /// <summary>
+        /// Die Funktion liest die CSV mit dem übergebenen Namen von Typ String ein und speichert das Datum 
+        /// und den Namen des Feiertags in einem MHoliday Objekt.
+        /// Dieses Objekt wird in einer List gespeichert und am Ende wird die Liste returned
+        /// </summary>
+        /// <param name="region"></param>
+        /// <returns></returns>
         public List<MHoliday> GetHoliday(String region)
         {
-            List<MHoliday> holidayArray = new List<MHoliday>();
-
-            return holidayArray;
-
-        }
-
-  /*      private bool openCSV(String path)
-        {
+            List<MHoliday> holidayList = new List<MHoliday>();
             StreamReader file =
-                new StreamReader(path);
-            while (file.ReadLine() != null)
-            {
-                String[] values = file.ReadLine().Split(";");
-                MHoliday mHoliday = new MHoliday();
-            }
+                new StreamReader("\\CSV\\"+ region +"2015.csv");
 
-            return true;
+            if (file.ReadLine() != null)
+            {
+                file.ReadLine().Split(';');
+                while (file.ReadLine() != null)
+                {
+                    String[] values = file.ReadLine().Split(';');
+                    MHoliday mHoliday = new MHoliday(Convert.ToDateTime(values[0]), values[1]);
+                    Console.WriteLine(mHoliday.holidayDate);
+                    holidayList.Add(mHoliday);
+                }
+            }
+               file.Close();
+
+               return holidayList;
+
         }
-    }*/
+
+    }
    
 }
