6c4f12664773aea998bd7946b0c2fe4dc38247e2
nwazal93@gmail.com <nwazal93@gmail.com@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-07 14:08:10 +0000

    Erstellent von:      MCalendarDay      MPractice      MSchool      MSpeciality Hinzufügen von Comment in     MHoliday

---
+++ trunk/Tagplaner/Tagplaner/Model/MSchool.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tagplaner
+{
+    public class MSchool
+    {
+        private String comment { get; set; }
+
+        public MSchool(String comment)
+        {
+            this.comment = comment;
+        }
+    }
+}
---
+++ trunk/Tagplaner/Tagplaner/Model/MSpeciality.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tagplaner
+{
+    public class MSpeciality
+    {
+        private String specialityName {get ; set;}
+        private String year { get; set; }
+        private String region { get; set; }
+
+        public MSpeciality(String specialityName, String year, String region)
+        {
+            this.specialityName = specialityName;
+            this.year = year;
+            this.region = region;
+        }
+    }
+}
