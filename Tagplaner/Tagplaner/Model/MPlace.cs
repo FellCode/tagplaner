f5010b0dc298da8450f6ef41d9e957fa93aeb063
sniperbill94 <sniperbill94@web.de>
2015-01-07 14:08:53 +0000

    Model-Klassen

---
+++ Tagplaner/Tagplaner/Model/MPlace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tagplaner
+{
+    class MPlace
+    {
+        private string place { get; set; }
+        private string contact { get; set; }
+        private List<MRoom> rooms { get; set; }
+    }
+}
---
+++ Tagplaner/Tagplaner/Model/MTrainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tagplaner
+{
+    class MTrainer
+    {
+        private string name { get; set; }
+        private string surname { get; set; }
+        private string abbreviation { get; set; }
+        private bool internally { get; set; }
+        private bool cotrainer { get; set; }
+    }
+}
