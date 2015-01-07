6df058a6e1d72f349f281012958b116555984b5f
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-07 11:44:33 +0000

    - Initialversion der Klasse PdfExporter

---
+++ trunk/Tagplaner/Tagplaner/Controller/PdfExporter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tagplaner
+{
+    class PdfExporter : IPdfExporter
+    {
+        public bool exportPdf(object[] objects, string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
