170d181adff35b536a268bcb5bcd532fb95160ca
max.thill <max.thill@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-08 10:46:07 +0000

    -TagplanAnlegenUserControl angepasst -TagplanBearbeitenUserControl TabControl hinzugefügt

--- trunk/Tagplaner/Tagplaner/View/TagplanAnlegenUserControl.cs
+++ trunk/Tagplaner/Tagplaner/View/TagplanAnlegenUserControl.cs
@@ -12,8 +12,11 @@ namespace Tagplaner
 {
     public partial class TagplanAnlegenUserControl : UserControl
     {
-        public TagplanAnlegenUserControl()
+        private FormInit formInit;
+
+        public TagplanAnlegenUserControl(FormInit formInit)
         {
+            this.formInit = formInit;    
             InitializeComponent();
         }
 
@@ -46,6 +49,46 @@ namespace Tagplaner
         private void groupBox1_Enter(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            System.IO.Directory.CreateDirectory(@"C:\Tagplan");
+
+            saveFileDialog1.InitialDirectory = @"C:\Tagplan";
+            saveFileDialog1.Filter = "Tagplan|*.tp";
+            saveFileDialog1.Title = "Tagplan abspeichern";
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = @"C:\Tagplan";
+            openFileDialog1.Title = "Tagplan öffnen";
+            openFileDialog1.Filter = "Tagplan|*.tp";
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.ShowDialog();
+            string[] result = openFileDialog1.FileNames;
+
+            foreach (string y in result)
+            {
+                System.IO.StreamReader sr = new
+               System.IO.StreamReader(y);
+                MessageBox.Show(sr.ReadToEnd());
+                sr.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nextTabPage();   
+        }
+
+        public void nextTabPage()
+        {
+            formInit.tabPageChange(1);
         }
     }
 }
