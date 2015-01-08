99c6a8c2eb27c83f1fe958d659afef67eedab180
arnoldbechtold4 <arnoldbechtold4@gmail.com>
2015-01-08 11:58:52 +0000

    Worksheet zur bearbeitung

--- Tagplaner/Tagplaner/Controller/WorksheetGenerator.cs
+++ Tagplaner/Tagplaner/Controller/WorksheetGenerator.cs
@@ -16,36 +16,7 @@ namespace Tagplaner
     {
 
         public WorksheetGenerator() { }
-        static void DoStuff()
-        {
-            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
 
-            #region error
-            if (xlApp == null)
-            {
-                Console.WriteLine("EXCEL konnte nicht gestartet werden :(");
-                return;
-            }
-            xlApp.Visible = true;
-
-            Workbook wb = xlApp.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
-            Worksheet ws = (Worksheet)wb.Worksheets[1];
-
-            if (ws == null)
-            {
-                Console.WriteLine("Worksheet konnte nicht erstellt werden :'(");
-            }
-
-
-            Range aRange = ws.get_Range("C3", "D7");
-
-            if (aRange == null)
-            {
-                Console.WriteLine("Zellen konnten nicht markiert werden");
-            }
-            #endregion
-
-            #region example
             /*aRange.Merge(Missing.Value);
             Object[] args = new Object[1];
 
@@ -61,13 +32,91 @@ namespace Tagplaner
             aRange.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
             aRange.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
             aRange.Value2 = "C# SEMINAR";*/
-
-            #endregion
-        }
-
-        public bool WriteFile(List<MCalendarEntry> calendar)
+     
+        public bool WriteFile(MCalendar calendar)
         {
-            throw new NotImplementedException();
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            string range = "A1";
+            string range1 = "A1";
+
+
+            #region error
+            if (xlApp == null)
+            {
+                Console.WriteLine("EXCEL konnte nicht gestartet werden :(");
+                return false;
+            }
+            xlApp.Visible = true;
+
+            Workbook wb = xlApp.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+            Worksheet ws = (Worksheet)wb.Worksheets[1];
+
+            if (ws == null)
+            {
+                Console.WriteLine("Worksheet konnte nicht erstellt werden :'(");
+                return false;
+            }
+
+
+            Range aRange = ws.get_Range(range, range1);
+
+            if (aRange == null)
+            {
+                Console.WriteLine("Zellen konnten nicht markiert werden");
+                return false;
+            }
+            #endregion
+
+            MCalendarEntry calendarEntry = calendar[0];
+            if (calendar[0].Holiday != null)
+            {
+                #region Holiday
+                DateTime calendarDate = calendarEntry.CalendarDay.CalendarDate;
+                string holidayName = calendarEntry.CalendarDay.HolidayName;
+                string calendarWeek = calendarEntry.CalendarDay.CalendarWeek;
+               
+                Object[] args = new Object[1];
+
+                args[0] = calendarDate;
+                aRange.set_Value("C1", "C1");
+                aRange.GetType().InvokeMember("Value", BindingFlags.SetProperty, null, aRange, args);
+                return true;
+                #endregion
+
+            }
+ /*           else if (calendar[0].School != null)
+            {
+                #region School
+                return true;
+                #endregion
+
+            }
+            else if (calendar[0].Practice != null)
+            {
+
+                if (calendar[0].Seminar != null)
+                {
+                    #region Practice/Seminar
+                    return true;
+                    #endregion
+
+                }
+                else
+                {
+                    #region Practice
+                    return true;
+                    #endregion
+                   
+                }
+
+            }
+            else
+            {
+                #region Seminar
+                return true;
+                #endregion
+            }*/
+
         }
 
     }
