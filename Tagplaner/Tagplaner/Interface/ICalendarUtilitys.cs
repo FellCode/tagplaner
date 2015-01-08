d1dc74d01230ac350c88ad9f067c24c7e8887c76
holler.christopher <holler.christopher@googlemail.com>
2015-01-08 14:15:36 +0000

    Interface für die Klasse CalendarUtilitys

---
+++ Tagplaner/Tagplaner/Interface/ICalendarUtilitys.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: tbender2
+ * Date: 07.01.2015
+ * Time: 16:22
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Tagplaner
+{
+	/// <summary>
+	/// Description of Interface1.
+	/// </summary>
+    public interface ICalendarUtilitys
+	{
+		List<MCalendarDay> generateCalenderDayEntrys();
+	}
+}
