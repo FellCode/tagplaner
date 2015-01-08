eeec87ca3aae751f5215e35a72a2532a64a22108
holler.christopher@googlemail.com <holler.christopher@googlemail.com@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-08 15:24:13 +0000

    Interface für automatisches befüllen der Feiertage hinzugefügt

---
+++ trunk/Tagplaner/Tagplaner/Interface/IICalCSVConverter.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: tbender2
+ * Date: 07.01.2015
+ * Time: 13:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Tagplaner
+{
+	/// <summary>
+	/// Description of Interface1.
+	/// </summary>
+	public interface IICalCSVConverter
+	{
+		List<MVacation> GetICalEntrys(DateTime startdate, DateTime enddate);
+	}
+}
