6feebce7d5ad0a5421d4d8d7e6a672334c8b7fd2
holler.christopher <holler.christopher@googlemail.com>
2015-01-08 14:41:05 +0000

    Überarbeitete MCalendarEntry

--- Tagplaner/Tagplaner/Model/MCalendarEntry.cs
+++ Tagplaner/Tagplaner/Model/MCalendarEntry.cs
@@ -8,110 +8,74 @@ namespace Tagplaner
 {
     [Serializable()]
     class MCalendarEntry
+    {
+        private MTrainer trainer;
+        private MTrainer cotrainer;
+        private MSpeciality speciality;
+        private MSeminar seminar;
+        private MPractice practice;
+        private MSchool school;
+        private MHoliday holiday;
+        private MPlace place;
+        private MRoom room;
+
+        #region getter
+        public MTrainer Trainer
         {
-            private MTrainer trainer;
-            private MTrainer cotrainer;
-            private MCalendarDay calendarDay;
-            private MSpeciality speciality;
-            private MSeminar seminar;
-            private MPractice practice;
-            private MSchool school;
-            private MHoliday holiday;
-            private MPlace place;
-            private MRoom room;
-
-            #region getter
-            public MTrainer Trainer
-            {
-                get { return trainer; }
-            }
-            public MTrainer Cotrainer
-            {
-                get { return cotrainer; }
-            }
-            public MCalendarDay CalendarDay
-            {
-                get { return calendarDay; }
-            }
-            public MSpeciality Speciality
-            {
-                get { return speciality; }
-            }
-            public MSeminar Seminar
-            {
-                get { return seminar; }
-            }
-            public MPractice Practice
-            {
-                get { return practice; }
-            }
-            public MSchool School
-            {
-                get { return school; }
-            }
-            public MHoliday Holiday
-            {
-                get { return holiday; }
-            }
-            public MPlace Place
-            {
-                get { return place; }
-            }
-            public MRoom Room
-            {
-                get { return room; }
-            }
-            #endregion
-
-            #region constructors
-            public MCalendarEntry(MTrainer trainer, MTrainer cotrainer, MCalendarDay calendarDay, MSpeciality speciality,
-                MSeminar seminar, MPlace place, MRoom room)
-            {
-                this.trainer = trainer;
-                this.cotrainer = cotrainer;
-                this.calendarDay = calendarDay;
-                this.speciality = speciality;
-                this.seminar = seminar;
-                this.place = place;
-                this.room = room;
-            }
-
-            public MCalendarEntry(MCalendarDay calendarDay, MSpeciality speciality, MPractice practice)
-            {
-                this.calendarDay = calendarDay;
-                this.speciality = speciality;
-                this.practice = practice;
-            }
-
-            public MCalendarEntry(MCalendarDay calendarDay, MSpeciality speciality, MSchool school)
-            {
-
-                this.calendarDay = calendarDay;
-                this.speciality = speciality;
-                this.school = school;
-            }
-
-            public MCalendarEntry(MCalendarDay calendarDay, MSpeciality speciality, MHoliday holiday)
-            {
-
-                this.calendarDay = calendarDay;
-                this.speciality = speciality;
-                this.holiday = holiday;
-            }
-
-            public MCalendarEntry(MTrainer trainer, MTrainer cotrainer, MCalendarDay calendarDay, MSpeciality speciality,
-                MSeminar seminar, MPlace place, MRoom room, MPractice practice)
-            {
-                this.trainer = trainer;
-                this.cotrainer = cotrainer;
-                this.calendarDay = calendarDay;
-                this.speciality = speciality;
-                this.seminar = seminar;
-                this.practice = practice;
-                this.place = place;
-                this.room = room;
-            }
-            #endregion
-
+            get { return trainer; }
         }
+        public MTrainer Cotrainer
+        {
+            get { return cotrainer; }
+        }
+        public MSpeciality Speciality
+        {
+            get { return speciality; }
+        }
+        public MSeminar Seminar
+        {
+            get { return seminar; }
+        }
+        public MPractice Practice
+        {
+            get { return practice; }
+        }
+        public MSchool School
+        {
+            get { return school; }
+        }
+        public MHoliday Holiday
+        {
+            get { return holiday; }
+        }
+        public MPlace Place
+        {
+            get { return place; }
+        }
+        public MRoom Room
+        {
+            get { return room; }
+        }
+        #endregion
+
+
+        public MCalendarEntry(MTrainer trainer, MTrainer cotrainer, MCalendarDay calendarDay, MSpeciality speciality,
+            MSeminar seminar, MPlace place, MRoom room)
+        {
+            this.trainer = trainer;
+            this.cotrainer = cotrainer;
+
+            this.speciality = speciality;
+            this.seminar = seminar;
+            this.place = place;
+            this.room = room;
+        }
+
+        public MCalendarEntry(MCalendarDay calendarDay, MSpeciality speciality, MPractice practice)
+        {
+
+            this.speciality = speciality;
+            this.practice = practice;
+        }
+    }
 }
