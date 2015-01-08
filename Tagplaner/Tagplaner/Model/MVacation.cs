ddba709d707ae5ee53b5894b1be6f5f4dbc0819b
holler.christopher <holler.christopher@googlemail.com>
2015-01-08 15:24:57 +0000

    Model MVacation hinzugefügt

---
+++ Tagplaner/Tagplaner/Model/MVacation.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: tbender2
+ * Date: 07.01.2015
+ * Time: 12:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Tagplaner
+{
+	/// <summary>
+	/// Description of MVacation.
+	/// </summary>
+	
+	public class MVacation
+    {
+        public String vacationName;
+        public DateTime vacationDate;
+
+        public MVacation(String vacationName, DateTime vacationDate)
+        {
+            this.vacationName = vacationName;
+            this.vacationDate = vacationDate;
+        }
+        
+		public string VacationName {
+			get { return vacationName; }
+			set { vacationName = value; }
+		}
+        
+        public DateTime VacationDate {
+			get { return vacationDate; }
+			set { vacationDate = value; }
+		}
+
+    }
+}
