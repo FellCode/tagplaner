a659248251c222e2d9b650bbef9b13999ff625a3
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-08 10:38:55 +0000

    - Initialversion TagplanBearbeitenUserControl

---
+++ Tagplaner/Tagplaner/View/TagplanBearbeitenUserControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tagplaner
+{
+    public partial class TagplanBearbeitenUserControl : UserControl
+    {
+        public TagplanBearbeitenUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void TagplanBearbeitenUserControl_Load(object sender, EventArgs e)
+        {
+            
+
+            for (int i = 1; i < 10; i++)
+            {
+                ListViewItem lv = new ListViewItem();
+                lv.Text = "0" + i + ".04.2015";
+                lv.SubItems.Add("");
+                lv.SubItems.Add("Osterferien");
+
+                listView1.Items.Add(lv);                 
+            }
+        }
+    }
+}
