c353884ec3ed6cb897fd5530e8da3a72861be014
holler.christopher <holler.christopher@googlemail.com>
2015-01-08 14:14:52 +0000

    MCalendar mit automatischer befüllung

--- Tagplaner/Tagplaner/Model/MCalendar.cs
+++ Tagplaner/Tagplaner/Model/MCalendar.cs
@@ -9,62 +9,39 @@ namespace Tagplaner
     [Serializable()]
     class MCalendar
     {
-        private List<MCalendarEntry> calendarList = new List<MCalendarEntry>();
+        private List<MCalendarDay> calendarList = new List<MCalendarDay>();
         private static MCalendar instance;
+        public string firstDayString { get; set; }
+        public string endDayString { get; set; }
 
         #region getter
-        public DateTime firstDay { get; set; }
-        public DateTime endDay { get; set; }
-        public List<MCalendarEntry> CalendarList
+        public DateTime startdate { get; set; }
+        public DateTime enddate { get; set; }
+        public List<MCalendarDay> CalendarList
         {
             get { return calendarList; }
         }
 
-        private MCalendar()
+        private MCalendar(DateTime start,DateTime end)
         {
+            startdate = start;
+            enddate = end;
 
+            CalendarUtilitys calendarUtilitys = new CalendarUtilitys(this.startdate, this.enddate, this.CalendarList);
+            calendarUtilitys.generateCalenderDayEntrys();
         }
 
-        public static MCalendar getInstance()
+        public static MCalendar getInstance(DateTime start, DateTime end)
         {
             if (instance == null)
             {
-                instance = new MCalendar();
+                instance = new MCalendar(start,end);
             } return instance;
         }
         #endregion
 
         #region AddDay-Methods
-        public void AddSeminarDay(MTrainer trainer, MTrainer cotrainer, MCalendarDay calendarDay, MSpeciality speciality, MSeminar seminar, MPlace place, MRoom room)
-        {
-            MCalendarEntry cal = new MCalendarEntry(trainer, cotrainer, calendarDay, speciality, seminar, place, room);
-            calendarList.Add(cal);
-        }
-
-        public void AddPracticeDay(MCalendarDay calendarDay, MSpeciality speciality, MPractice practice)
-        {
-            MCalendarEntry cal = new MCalendarEntry(calendarDay, speciality, practice);
-            calendarList.Add(cal);
-        }
-
-        public void AddSchoolDay(MCalendarDay calendarDay, MSpeciality speciality, MSchool school)
-        {
-            MCalendarEntry cal = new MCalendarEntry(calendarDay, speciality, school);
-            calendarList.Add(cal);
-        }
-
-        public void AddHoliday(MCalendarDay calendarDay, MSpeciality speciality, MHoliday holiday)
-        {
-            MCalendarEntry cal = new MCalendarEntry(calendarDay, speciality, holiday);
-            calendarList.Add(cal);
-        }
-
-        public void AddPracticeSeminarDay(MTrainer trainer, MTrainer cotrainer, MCalendarDay calendarDay, MSpeciality speciality, MSeminar seminar, MPlace place, MRoom room, MPractice practice)
-        {
-            MCalendarEntry cal = new MCalendarEntry(trainer, cotrainer, calendarDay, speciality, seminar, place, room, practice);
-            calendarList.Add(cal);
-        }
-
+     
 #endregion
         
     }
