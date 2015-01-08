d51c059a3c36842229563468b2732179a702478d
holler.christopher@googlemail.com <holler.christopher@googlemail.com@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-08 15:23:00 +0000

    Controller zum automatischen befüllen der Feiertage hinzugefügt

---
+++ trunk/Tagplaner/Tagplaner/Controller/ICalCSVConverter.cs
@@ -0,0 +1,166 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: tbender2
+ * Date: 07.01.2015
+ * Time: 09:46
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DDay.iCal;
+
+
+namespace Tagplaner
+{
+	/// <summary>
+	/// Konstruktor erwartet einen string mit der URL der ics-Datei
+	/// </summary>
+	
+	public class ICalCSVConverter: IICalCSVConverter
+	{
+		private string currentYearURL;
+		private string nextYearURL;		
+		private DateTime startdate;
+		private DateTime enddate;
+		private IICalendarCollection icalendar;
+		private string exceptionMessage;
+		private List<MVacation> vacationList;
+		
+		public ICalCSVConverter(string currentYearURL, string nextYearURL)
+		{
+			CurrentYearURL = currentYearURL;
+			NextYearURL = nextYearURL;
+			VacationList = new List<MVacation>();
+		}
+		
+		public List<MVacation> GetICalEntrys(DateTime startdate, DateTime enddate){
+			ReadICalEntrysCurrentYear(new DateTime(startdate.Year,startdate.Month,startdate.Day),new DateTime(startdate.Year,12,31));
+			ReadICalEntrysNextYear(new DateTime(enddate.Year,01,01),new DateTime(enddate.Year,enddate.Month,enddate.Day));
+			return VacationList;
+		}
+		
+		public void ReadICalEntrysCurrentYear(DateTime startdate, DateTime enddate){
+			try {
+				ICalendar= iCalendar.LoadFromFile(currentYearURL);
+			}
+			catch(Exception exception){
+				ExceptionMessage = exception.Message;
+				return;
+			}
+			
+			Startdate = startdate;
+			
+			IList<Occurrence> occurrences = icalendar.GetOccurrences
+				(startdate,enddate);
+
+			Console.WriteLine("Entrys from " + Startdate.ToString() + " >");
+
+			foreach (Occurrence occurrence in occurrences)
+			{
+				//DateTime occurrenceTime = occurrence.Period.StartTime.Local;
+				IRecurringComponent rc = occurrence.Source as IRecurringComponent;
+				IEvent evt = occurrence.Source as IEvent;
+				//MHoliday mHoliday = new MHoliday(evt.Start.Local, evt.End.Local, evt.Summary);
+				//HolidayList.Add(mHoliday);
+				GenerateMVacationCurrentYear(evt.Start.Local, evt.End.Local, evt.Summary);
+				
+			}
+			//return VacationList;
+		}
+		
+		public void ReadICalEntrysNextYear(DateTime startdate, DateTime enddate){
+			
+			try {
+				ICalendar= iCalendar.LoadFromFile(nextYearURL);
+			}
+			catch(Exception exception){
+				ExceptionMessage = exception.Message;
+				return;
+			}
+			
+			Enddate = enddate;
+			
+			IList<Occurrence> occurrences = icalendar.GetOccurrences
+				(startdate,enddate);
+
+			Console.WriteLine("Entrys until " + enddate.ToString() + " >");
+
+			foreach (Occurrence occurrence in occurrences)
+			{
+				//DateTime occurrenceTime = occurrence.Period.StartTime.Local;
+				IRecurringComponent rc = occurrence.Source as IRecurringComponent;
+				IEvent evt = occurrence.Source as IEvent;
+				//MHoliday mHoliday = new MHoliday(evt.Start.Local, evt.End.Local, evt.Summary);
+				//HolidayList.Add(mHoliday);
+				GenerateMVacationNextYear(evt.Start.Local, evt.End.Local, evt.Summary);
+				
+			}
+			//return VacationList;
+		}
+		
+		private void GenerateMVacationCurrentYear(DateTime start, DateTime end, string vacationName){
+			for(var day = start; day.Date <= end; day = day.AddDays(1)){				
+				MVacation mVacation = null;
+				if(day.Date >= Startdate.Date) {
+					mVacation= new MVacation(vacationName, day);
+					VacationList.Add(mVacation);
+					Console.Write("*");
+				}
+				Console.WriteLine(vacationName);
+				Console.WriteLine(day);
+			}
+		}
+		
+		private void GenerateMVacationNextYear(DateTime start, DateTime end, string vacationName){
+			for(var day = start; day.Date <= end; day = day.AddDays(1)){				
+				MVacation mVacation = null;
+				if(day.Date <= Enddate.Date) {
+					mVacation= new MVacation(vacationName, day);
+					VacationList.Add(mVacation);
+					Console.Write("*");
+				}
+				Console.WriteLine(vacationName);
+				Console.WriteLine(day);
+			}
+		}
+		
+		public string CurrentYearURL {
+			get { return currentYearURL; }
+			set { currentYearURL = value; }
+		}
+		
+		public string NextYearURL {
+			get { return nextYearURL; }
+			set { nextYearURL = value; }
+		}
+
+		
+		public DateTime Startdate {
+			get { return startdate; }
+			set { startdate = value; }
+		}
+
+		public DateTime Enddate {
+			get { return enddate; }
+			set { enddate = value; }
+		}
+		
+		public IICalendarCollection ICalendar {
+			get { return icalendar; }
+			set { icalendar = value; }
+		}
+				
+		public string ExceptionMessage {
+			get { return exceptionMessage; }
+			set { exceptionMessage = value; }
+		}
+		
+		public List<MVacation> VacationList {
+			get { return vacationList; }
+			set { vacationList = value; }
+		}
+		
+	}
+}
