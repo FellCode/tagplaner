e4551badf80cad484bc560c4b971dac25244e164
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-09 13:13:30 +0000

    - ICalCSVConverter in CICalCSVConverter umbenannt - CalendarUtilitys in CCalendarUtilitys umbenannt

---
+++ trunk/Tagplaner/Tagplaner/Controller/CCalendarUtilitys.cs
@@ -0,0 +1,68 @@
+/*
+ * Created by SharpDevelop.
+ * User: tbender2
+ * Date: 07.01.2015
+ * Time: 16:01
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Tagplaner
+{
+	/// <summary>
+	/// Description of GenerateCalendarDays.
+	/// </summary>
+    public class CCalendarUtilitys : ICalendarUtilitys
+	{
+		private DateTime startdate;
+		private DateTime enddate;
+        private List<MCalendarDay> tempCalendarDay;
+
+        public CCalendarUtilitys(DateTime startdate, DateTime enddate, List<MCalendarDay> mCalendarDay)
+		{
+			Startdate = startdate;
+			Enddate = enddate;
+            TempCalendarDay = mCalendarDay;
+		}
+
+        public List<MCalendarDay> generateCalenderDayEntrys()
+        {
+            DateTimeFormatInfo dfi = DateTimeFormatInfo.CurrentInfo;
+            Calendar calendar = dfi.Calendar;
+
+			for(var day = Startdate; day.Date <= Enddate; day = day.AddDays(1)){
+				//Console.WriteLine(day.Date);
+                Console.WriteLine("{0:d}: Week {1}", day.Date,
+				                  calendar.GetWeekOfYear(day.Date, dfi.CalendarWeekRule,
+				                                    dfi.FirstDayOfWeek));
+				//CalendarDays.Add(day);
+                MCalendarDay mCalendarDay = new MCalendarDay(day, null, null, calendar.GetWeekOfYear(day.Date, dfi.CalendarWeekRule,
+                                                    dfi.FirstDayOfWeek).ToString());
+                TempCalendarDay.Add(mCalendarDay);
+                //MCalendarDay.Add(new MCalendarDay();
+                
+			}
+            return TempCalendarDay;
+		}
+		
+		public DateTime Startdate {
+			get { return startdate; }
+			set { startdate = value; }
+		}
+
+		public DateTime Enddate {
+			get { return enddate; }
+			set { enddate = value; }
+		}
+
+        public List<MCalendarDay> TempCalendarDay
+        {
+            get { return tempCalendarDay; }
+            set { tempCalendarDay = value; }
+		}
+
+	}
+}
