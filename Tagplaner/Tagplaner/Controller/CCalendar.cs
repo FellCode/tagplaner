15262868cc7c9639baf309088b7575d2f0d78c1a
holler.christopher <holler.christopher@googlemail.com>
2015-01-09 12:16:28 +0000

    Kommentare hinzugefügt

--- Tagplaner/Tagplaner/Controller/CCalendar.cs
+++ Tagplaner/Tagplaner/Controller/CCalendar.cs
@@ -8,12 +8,19 @@ namespace Tagplaner
 {
     public class CCalendar
     {
+        /// <summary>
+        /// Funktion zum füllen der Tage vom Startdatum bis zum Enddatum sowie den dazugehörigen Ferien.
+        /// </summary>
+        /// <param name="startdate">Beginn des Kalenders</param>
+        /// <param name="enddate">Ende des Kalenders</param>
+        /// <param name="listDays">Liste der Tage von start bis ende</param>
+        /// <returns></returns>
         public List<MCalendarDay> fillDaysInitial(DateTime startdate,DateTime enddate,List<MCalendarDay> listDays)
         {
             CalendarUtilitys calendarUtilitys = new CalendarUtilitys(startdate, enddate, listDays);
             calendarUtilitys.generateCalenderDayEntrys();
 
-            ICalCSVConverter iCalCSVConverter = new ICalCSVConverter("C:\\Users\\choller\\Documents\\Visual Studio 2013\\Projects\\Tagplaner\\Tagplaner\\Tagplaner\\Ferien_Hessen_2015.ics", "C:\\Users\\choller\\Documents\\Visual Studio 2013\\Projects\\Tagplaner\\Tagplaner\\Tagplaner\\Ferien_Hessen_2016.ics");
+            ICalCSVConverter iCalCSVConverter = new ICalCSVConverter("CSV\\Ferien_Hessen_2015.ics", "CSV\\Ferien_Hessen_2016.ics");
             foreach (MVacation vacation in iCalCSVConverter.GetICalEntrys(startdate, enddate))
             {
                 foreach (MCalendarDay day in listDays)
@@ -27,6 +34,13 @@ namespace Tagplaner
             return listDays;
         }
 
+        /// <summary>
+        /// Funktion zum hinzufügen der Feiertage in die Tagliste
+        /// </summary>
+        /// <param name="startdate">Beginn des Kalenders</param>
+        /// <param name="enddate">Ende des Kalenders</param>
+        /// <param name="listDays">Liste der Tage von start bis ende</param>
+        /// <returns></returns>
         public List<MCalendarDay> fillHolidaysInitial(DateTime startdate, DateTime enddate, List<MCalendarDay> listDays)
         {
             CHoliday holiday = new CHoliday();
