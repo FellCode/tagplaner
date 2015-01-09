9ab5c729966d8fe15c0f7fe36bcaf21899245c78
lf2freek <lf2freek@gmail.com>
2015-01-09 12:09:24 +0000

    neue funktionen hinzugefügt

--- Tagplaner/Tagplaner/Interface/IDatabase.cs
+++ Tagplaner/Tagplaner/Interface/IDatabase.cs
@@ -13,12 +13,19 @@ namespace Tagplaner
         bool ConnectDatabase();
         bool CloseDatabase();
 
+        bool InsertSeminar(string titel, string untertitel, string kuerzel, string technick);
+        bool InsertTrainer(string vorname, string nachname, string kuerzel, string intern);
+        bool InsertRoom(string raumnummer, string fk_seminarort_id);
+        bool InsertLocation(string ort, string ansprechpartner, string fk_bundesland_id);
+        bool InsertFederelState(string name, string kuerzel);
+
+
         void FillTrainerCombobox(ComboBox combobox);
         void FillSeminarCombobox(ComboBox combobox);
         void FillFederalStateCombobox(ComboBox combobox);
         void FillLocationCombobox(ComboBox combobox);
         //id des Seminarorts mitgeben
-        void FillRoomCombobox(ComboBox combobox, int location);
+        void FillRoomCombobox(ComboBox combobox, string location);
 
         //Könnte man in Select, Update und delete aufteilen
         SQLiteDataReader ExecuteQuery(string query);
