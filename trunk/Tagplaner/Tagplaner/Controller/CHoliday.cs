8e076eb6d35712f5a09b4840fb19e473f785c646
F.Smudo@gmail.com <F.Smudo@gmail.com@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-09 09:50:21 +0000

    Code optimiert

--- trunk/Tagplaner/Tagplaner/Controller/CHoliday.cs
+++ trunk/Tagplaner/Tagplaner/Controller/CHoliday.cs
@@ -15,6 +15,7 @@ namespace Tagplaner
     class CHoliday : IHoliday
     {
         private List<MHoliday> holidayList = new List<MHoliday>();
+
         /// <summary>
         /// Die Funktion liest die CSV mit dem übergebenen Namen von Typ String ein und speichert das Datum 
         /// und den Namen des Feiertags in einem MHoliday Objekt.
@@ -24,48 +25,55 @@ namespace Tagplaner
         /// <returns></returns>
         public List<MHoliday> GetHoliday(String region, DateTime startDate, DateTime endDate)
         {
-          /*  string remoteUri = "http://www.feiertage.net/csvfile.php?state=NW&year=2017&type=csv";
-            string fileName = "Nordrhein-Westfalen2017.csv", myStringWebResource = null;
+            /*  string remoteUri = "http://www.feiertage.net/csvfile.php?state=NW&year=2017&type=csv";
+              string fileName = "Nordrhein-Westfalen2017.csv", myStringWebResource = null;
 
-            // Create a new WebClient instance.
-            using (WebClient myWebClient = new WebClient())
-            {
-                myStringWebResource = remoteUri + fileName;
-                // Download the Web resource and save it into the current filesystem folder.
-                myWebClient.DownloadFile(myStringWebResource, fileName);
-            } */
-         
-            StreamReader file =
-                new StreamReader("CSV\\" + region + startDate.Year +".csv");
-            if (file.ReadLine() != null)
-            {
-                while (!file.EndOfStream)
-                {
-                    String[] values = file.ReadLine().Split(';');
-                    if (Convert.ToDateTime(values[0]) >= startDate)
-                        AddHoliday(values);
-                }
-                file = new StreamReader("CSV\\" + region + endDate.Year + ".csv");
-                if (file.ReadLine() != null)
-                {
-                    while (!file.EndOfStream)
-                    {
-                        String[] values = file.ReadLine().Split(';');
-                        if (Convert.ToDateTime(values[0]) <= endDate)
-                            AddHoliday(values);
-                        
-                    }
-                }
-            }
+              // Create a new WebClient instance.
+              using (WebClient myWebClient = new WebClient())
+              {
+                  myStringWebResource = remoteUri + fileName;
+                  // Download the Web resource and save it into the current filesystem folder.
+                  myWebClient.DownloadFile(myStringWebResource, fileName);
+              } */
+
+            StreamReader file = new StreamReader("CSV\\" + region + startDate.Year + ".csv");
+            ValidateHoliday(file, startDate, endDate);
+
+            file = new StreamReader("CSV\\" + region + endDate.Year + ".csv");
+            ValidateHoliday(file, startDate, endDate);
             file.Close();
-             
+
             return holidayList;
+
         }
+        /// <summary>
+        /// Fügt die Validierten Feiertage der Liste hinzu
+        /// </summary>
+        /// <param name="values"></param>
         public void AddHoliday(String[] values)
         {
             MHoliday mHoliday = new MHoliday(Convert.ToDateTime(values[0]), values[1]);
             holidayList.Add(mHoliday);
             Console.WriteLine(values[0]);
+        }
+        /// <summary>
+        /// Validiert die eingelesenen Feiertage auf den angegebenen Tagplan Zeitraum
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        public void ValidateHoliday(StreamReader file, DateTime startDate, DateTime endDate)
+        {
+            if (file.ReadLine() != null)
+            {
+                while (!file.EndOfStream)
+                {
+                    String[] values = file.ReadLine().Split(';');
+                    if (Convert.ToDateTime(values[0]) >= startDate && Convert.ToDateTime(values[0]) <= endDate)
+                        AddHoliday(values);
+                }
+            }
+
         }
     }
 }
