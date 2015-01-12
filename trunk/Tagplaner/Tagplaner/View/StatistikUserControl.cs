3122e6997d583a01582723674e886f8b1d3d768c
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-12 12:55:29 +0000

    - ListView aktusalisieren in eigene Methode ausgelagert RefreshListView()

--- trunk/Tagplaner/Tagplaner/View/StatistikUserControl.cs
+++ trunk/Tagplaner/Tagplaner/View/StatistikUserControl.cs
@@ -25,38 +25,62 @@ namespace Tagplaner
         private void button1_Click(object sender, EventArgs e)
         {
             RefreshListView();
-
         }
 
-        private void RefreshListView()
+        public void RefreshListView() {
+            listView1.Items.Clear();
+
+            listView1.Items.Add(GetAllDaysListViewItem());
+            listView1.Items.Add(GetApprenticeshipDaysListViewItem());
+            listView1.Items.Add(GetWeekendDaysListViewItem());
+            listView1.Items.Add(GetHolidayDaysListViewItem());
+        }
+
+        private ListViewItem GetAllDaysListViewItem()
+        {
+            int allDays = CStatisticUtilitys.CountAllApprenticeshipDays();
+
+            ListViewItem lvAllDays = new ListViewItem();
+            lvAllDays.Text = "Tage gesamt";
+            lvAllDays.SubItems.Add(Convert.ToString(allDays));
+
+            return lvAllDays;
+        }
+
+        private ListViewItem GetApprenticeshipDaysListViewItem()
         {
             int allDays = CStatisticUtilitys.CountAllApprenticeshipDays();
             int weekendDays = CStatisticUtilitys.CountWeekendDays();
             int holidayDays = CStatisticUtilitys.CountHolidayDays();
             int apprenticeshipDays = allDays - weekendDays - holidayDays;
 
-            listView1.Items.Clear();
-
-            ListViewItem lvAllDays = new ListViewItem();
-            lvAllDays.Text = "Tage gesamt";
-            lvAllDays.SubItems.Add(Convert.ToString(allDays));
-
             ListViewItem lvApprenticeshipDays = new ListViewItem();
             lvApprenticeshipDays.Text = "Tage ohne Wochenenden und Feiertage";
             lvApprenticeshipDays.SubItems.Add(Convert.ToString(apprenticeshipDays));
+
+            return lvApprenticeshipDays;
+        }
+
+        private ListViewItem GetWeekendDaysListViewItem()
+        {
+            int weekendDays = CStatisticUtilitys.CountWeekendDays();
 
             ListViewItem lvWeekendDays = new ListViewItem();
             lvWeekendDays.Text = "Samstage und Sonntage";
             lvWeekendDays.SubItems.Add(Convert.ToString(weekendDays));
 
+            return lvWeekendDays;
+        }
+
+        private ListViewItem GetHolidayDaysListViewItem()
+        {
+            int holidayDays = CStatisticUtilitys.CountHolidayDays();
+
             ListViewItem lvHolidayDays = new ListViewItem();
             lvHolidayDays.Text = "Feiertage";
             lvHolidayDays.SubItems.Add(Convert.ToString(holidayDays));
 
-            listView1.Items.Add(lvAllDays);
-            listView1.Items.Add(lvApprenticeshipDays);
-            listView1.Items.Add(lvWeekendDays);
-            listView1.Items.Add(lvHolidayDays);
+            return lvHolidayDays;
         }
     }
 }
