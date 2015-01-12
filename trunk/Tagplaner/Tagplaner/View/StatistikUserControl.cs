fa3a329570d211fd0ec8e47c63ad152863d8634e
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-12 09:22:18 +0000

    - Methoden zum Berechnen von Anzahl Wochenend-, Feier-, Seminar-, Schul- und Praixstagen hinzugefügt

--- trunk/Tagplaner/Tagplaner/View/StatistikUserControl.cs
+++ trunk/Tagplaner/Tagplaner/View/StatistikUserControl.cs
@@ -17,20 +17,36 @@ namespace Tagplaner
             InitializeComponent();
         }
 
-        private void StatistikUserControl_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
+            int allDays = CStatisticUtilitys.CountAllApprenticeshipDays();
+            int weekendDays = CStatisticUtilitys.CountWeekendDays();
+            int holidayDays = CStatisticUtilitys.CountHolidayDays();
+            int apprenticeshipDays = allDays - weekendDays - holidayDays;
+
             listView1.Items.Clear();
 
-            ListViewItem lv = new ListViewItem();
-            lv.Text = "Tage gesamt";
-            lv.SubItems.Add(Convert.ToString(MCalendar.getInstance().CalendarList.Count));
+            ListViewItem lvAllDays = new ListViewItem();
+            lvAllDays.Text = "Tage gesamt";
+            lvAllDays.SubItems.Add(Convert.ToString(allDays));
 
-            listView1.Items.Add(lv);
+            ListViewItem lvApprenticeshipDays = new ListViewItem();
+            lvApprenticeshipDays.Text = "Tage ohne Wochenenden und Feiertage";
+            lvApprenticeshipDays.SubItems.Add(Convert.ToString(apprenticeshipDays));
+
+            ListViewItem lvWeekendDays = new ListViewItem();
+            lvWeekendDays.Text = "Samstage und Sonntage";
+            lvWeekendDays.SubItems.Add(Convert.ToString(weekendDays));
+
+            ListViewItem lvHolidayDays = new ListViewItem();
+            lvHolidayDays.Text = "Feiertage";
+            lvHolidayDays.SubItems.Add(Convert.ToString(holidayDays));
+
+            listView1.Items.Add(lvAllDays);
+            listView1.Items.Add(lvApprenticeshipDays);
+            listView1.Items.Add(lvWeekendDays);
+            listView1.Items.Add(lvHolidayDays);
+
         }
     }
 }
