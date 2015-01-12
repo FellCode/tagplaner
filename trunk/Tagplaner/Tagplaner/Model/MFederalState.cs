12176c085096303783c7bd7d0161dc69f73e44e9
dvm229000@gmail.com <dvm229000@gmail.com@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-12 12:28:33 +0000

    Model für bundesland

---
+++ trunk/Tagplaner/Tagplaner/Model/MFederalState.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tagplaner
+{
+    class MFederalState
+    {   
+        private int id;
+        private string name;
+        private string abbreviation;
+
+        #region getter_setter
+        public int Id 
+        {
+            get { return id; }
+            set { this.id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+
+        public string Abbreviation
+        {
+            get { return abbreviation; }
+            set { this.abbreviation = value; }
+        }
+        #endregion
+
+        public MFederalState(int id, string name, string abbreviation)
+        {
+            this.id = id;
+            this.name = name;
+            this.abbreviation = abbreviation;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+    }
+}
