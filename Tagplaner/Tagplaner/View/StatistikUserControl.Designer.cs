c106b3338d9b15b8db3fb12cee93e53130870512
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-12 09:22:18 +0000

    - Methoden zum Berechnen von Anzahl Wochenend-, Feier-, Seminar-, Schul- und Praixstagen hinzugefügt

--- Tagplaner/Tagplaner/View/StatistikUserControl.Designer.cs
+++ Tagplaner/Tagplaner/View/StatistikUserControl.Designer.cs
@@ -72,7 +72,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(412, 3);
+            this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -88,7 +88,6 @@
             this.Controls.Add(this.listView1);
             this.Name = "StatistikUserControl";
             this.Size = new System.Drawing.Size(490, 311);
-            this.Load += new System.EventHandler(this.StatistikUserControl_Load);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
