92d9bce21a272ac29be6322d51508efaec2a1a3c
i.pfeuster <i.pfeuster@gmail.com>
2015-01-12 08:49:24 +0000

    Hinzufügen des Seminar verwalten Tabs

---
+++ Tagplaner/Tagplaner/View/SeminarVerwaltenUserControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tagplaner
+{
+    public partial class SeminarVerwaltenUserControl : UserControl
+    {
+        CDatabase db = new CDatabase();
+        
+        public SeminarVerwaltenUserControl()
+        {
+            db.CreateDB();
+            db.FillDB();
+            InitializeComponent();
+            
+         db.FillSeminarCombobox(seminarBox);
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void seminarBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+      
+    }
+}
