ff3e666b2255cf59a2a3c51518cf68033a462f7e
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-12 09:24:02 +0000

    - Methoden zum Berechnen von Anzahl Wochenend-, Feier-, Seminar-, Schul- und Praixstagen hinzugefügt

--- Tagplaner/Tagplaner/Controller/CStatisticUtilitys.cs
+++ Tagplaner/Tagplaner/Controller/CStatisticUtilitys.cs
@@ -4,10 +4,97 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tagplaner.Controller
+namespace Tagplaner
 {
     class CStatisticUtilitys
     {
+        private CStatisticUtilitys() { }
 
+        /// <summary>
+        /// Zählt die Anzahl alle Tage zwischen dem Start- und Enddatum der MCalendar Instanz
+        /// und gibt diese zurück
+        /// </summary>
+        /// <returns></returns>
+        public static int CountAllApprenticeshipDays()
+        {
+            return MCalendar.getInstance().CalendarList.Count();
+        }
+
+        /// <summary>
+        /// Zählt alle Wochenendtage zwischen dem Start- und Enddatum der MCalendar Instanz
+        /// und gibt diese zurück
+        /// </summary>
+        /// <returns></returns>
+        public static int CountWeekendDays()
+        {
+            MCalendarDay calendarDay = null;
+            int weekendCounter = 0;
+
+            for (int i = 0; i < MCalendar.getInstance().CalendarList.Count(); i++)
+            {
+                calendarDay = MCalendar.getInstance().CalendarList.ElementAt(i);
+
+                if (calendarDay.CalendarDate.DayOfWeek.ToString().Equals("Saturday") ||
+                    calendarDay.CalendarDate.DayOfWeek.ToString().Equals("Sunday"))
+                {
+                    weekendCounter++;
+                }
+            }
+
+            return weekendCounter;
+        }
+
+        /// <summary>
+        /// Zählt alle Feiertage zwischen dem Start- und Enddatum der MCalendar Instanz
+        /// und gibt diese zurück
+        /// </summary>
+        /// <returns></returns>
+        public static int CountHolidayDays()
+        {
+            MCalendarDay calendarDay = null;
+            int holidayCounter = 0;
+
+            for (int i = 0; i < MCalendar.getInstance().CalendarList.Count(); i++)
+            {
+                calendarDay = MCalendar.getInstance().CalendarList.ElementAt(i);
+
+                if (!String.IsNullOrEmpty(calendarDay.HolidayName))
+                {
+                    holidayCounter++;
+                }
+            }
+
+            return holidayCounter;
+        }
+
+        /// <summary>
+        /// Zählt alle Seminartage zwischen dem Start- und Enddatum der MCalendar Instanz
+        /// und gibt diese zurück
+        /// </summary>
+        /// <returns></returns>
+        public static int CountSeminarDays()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Zählt alle Schultage zwischen dem Start- und Enddatum der MCalendar Instanz
+        /// und gibt diese zurück 
+        /// </summary>
+        /// <returns></returns>
+        public static int CountSchoolDays()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Zählt alle Praxistage  zwischen dem Start- und Enddatum der MCalendar Instanz
+        /// und gibt diese zurück
+        /// </summary>
+        /// <returns></returns>
+        public static int CountPraticeDays()
+        {
+            return 0;
+        }
     }
 }
