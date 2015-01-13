22d606709f82a892f1bfa9c4ffe274bdb52839c0
i.pfeuster@gmail.com <i.pfeuster@gmail.com@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-13 08:46:48 +0000

    Raum verwalten hinzugefügt

---
+++ trunk/Tagplaner/Tagplaner/View/RaumVerwaltenUserControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tagplaner
+{
+    public partial class RaumVerwaltenUserControl : UserControl
+    {
+        CDatabase db = new CDatabase();
+
+        public RaumVerwaltenUserControl()
+        {
+            InitializeComponent();
+            db.FillLocationCombobox(comboBox2);
+        }
+
+        private void RaumVerwaltenUserControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             MPlace place = (MPlace) comboBox2.SelectedItem;
+            db.InsertRoom(textBox1.Text, place.Id.ToString()); 
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+           MPlace place = (MPlace) comboBox2.SelectedItem;
+             db.FillRoomCombobox(comboBox1, place.Id);
+        }
+    }
+}
