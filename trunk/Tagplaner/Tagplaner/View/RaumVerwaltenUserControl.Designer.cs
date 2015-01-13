6c20248ee933b8937532a99b585dc1b40ea090bf
i.pfeuster@gmail.com <i.pfeuster@gmail.com@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-13 13:12:12 +0000

    Trainer verrwalten und Raum verwalten angepasst

--- trunk/Tagplaner/Tagplaner/View/RaumVerwaltenUserControl.Designer.cs
+++ trunk/Tagplaner/Tagplaner/View/RaumVerwaltenUserControl.Designer.cs
@@ -34,7 +34,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
@@ -96,7 +95,6 @@
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.button2);
             this.groupBox3.Controls.Add(this.button4);
             this.groupBox3.Controls.Add(this.button3);
             this.groupBox3.Controls.Add(this.button1);
@@ -111,18 +109,9 @@
             this.groupBox3.Text = "Raum bearbeiten";
             this.groupBox3.Enter += new System.EventHandler(this.groupBox2_Enter);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(88, 71);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Ändern";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(250, 71);
+            this.button4.Location = new System.Drawing.Point(169, 71);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(88, 23);
             this.button4.TabIndex = 5;
@@ -132,12 +121,13 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(169, 71);
+            this.button3.Location = new System.Drawing.Point(88, 71);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 4;
             this.button3.Text = "Löschen";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
             // 
             // button1
             // 
@@ -193,7 +183,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label3;
 
