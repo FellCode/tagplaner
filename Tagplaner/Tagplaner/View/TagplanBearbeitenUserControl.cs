e46cc6b76d67caa5e9000608a2632936ff43a8dc
max.thill <max.thill@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-13 14:05:03 +0000

    -Anpassungen an der TagplanBearbeitenUserControl

--- Tagplaner/Tagplaner/View/TagplanBearbeitenUserControl.cs
+++ Tagplaner/Tagplaner/View/TagplanBearbeitenUserControl.cs
@@ -14,19 +14,27 @@ namespace Tagplaner
 {
     public partial class TagplanBearbeitenUserControl : UserControl
     {
-        
+        private DataGridView dGV;
+
         public TagplanBearbeitenUserControl()
         {
             InitializeComponent();
+            dGV = new DataGridView();
         }
 
         private void TagplanBearbeitenUserControl_Load(object sender, EventArgs e)
         {
-
+            CEditPlan cEditPlan = new CEditPlan();
+            dGV = cEditPlan.createDataGridViews(3);
+            this.Controls.Add(dGV);
         }
 
         public ListView GetListView(){
             return this.listView1;
+        }
+        public DataGridView dGVReturn()
+        {
+            return dGV;
         }
     }
 }
