62456f985eb52a3c0d3a5524361afeb26454a4ad
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-14 09:47:19 +0000

    - Fenster für das exportieren von PDF Dokumenten hinzugefügt

---
+++ trunk/Tagplaner/Tagplaner/View/ExportPdfForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tagplaner
+{
+    public partial class ExportPdfForm : Form
+    {
+        public ExportPdfForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExportPdfForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
