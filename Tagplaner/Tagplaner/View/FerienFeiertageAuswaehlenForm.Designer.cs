c82a3f0003175c4ebd25a6d7642c4b44ff74a4a3
max.thill <max.thill@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-14 09:34:38 +0000

    -FerienFeiertageAuswählen angepasst

--- Tagplaner/Tagplaner/View/FerienFeiertageAuswaehlenForm.Designer.cs
+++ Tagplaner/Tagplaner/View/FerienFeiertageAuswaehlenForm.Designer.cs
@@ -46,7 +46,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(748, 15);
+            this.button1.Location = new System.Drawing.Point(248, 15);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(147, 23);
             this.button1.TabIndex = 0;
@@ -56,7 +56,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(748, 44);
+            this.button2.Location = new System.Drawing.Point(248, 44);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(147, 23);
             this.button2.TabIndex = 1;
@@ -66,7 +66,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(748, 73);
+            this.button3.Location = new System.Drawing.Point(248, 73);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(147, 23);
             this.button3.TabIndex = 2;
@@ -76,7 +76,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(748, 102);
+            this.button4.Location = new System.Drawing.Point(248, 102);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(147, 23);
             this.button4.TabIndex = 3;
@@ -89,7 +89,7 @@
             this.textBox1.Enabled = false;
             this.textBox1.Location = new System.Drawing.Point(12, 17);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(730, 20);
+            this.textBox1.Size = new System.Drawing.Size(230, 20);
             this.textBox1.TabIndex = 8;
             this.textBox1.Text = "Feiertagedatei des aktuellen Jahres auswählen";
             // 
@@ -98,7 +98,7 @@
             this.textBox2.Enabled = false;
             this.textBox2.Location = new System.Drawing.Point(12, 46);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(730, 20);
+            this.textBox2.Size = new System.Drawing.Size(230, 20);
             this.textBox2.TabIndex = 9;
             this.textBox2.Text = "Feiertagedatei des nächsten Jahres auswählen";
             // 
@@ -107,7 +107,7 @@
             this.textBox3.Enabled = false;
             this.textBox3.Location = new System.Drawing.Point(12, 74);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(730, 20);
+            this.textBox3.Size = new System.Drawing.Size(230, 20);
             this.textBox3.TabIndex = 10;
             this.textBox3.Text = "Feriendatei des aktuellen Jahres auswählen";
             // 
@@ -116,7 +116,7 @@
             this.textBox4.Enabled = false;
             this.textBox4.Location = new System.Drawing.Point(12, 104);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(730, 20);
+            this.textBox4.Size = new System.Drawing.Size(230, 20);
             this.textBox4.TabIndex = 11;
             this.textBox4.Text = "Feriendatei des nächsten Jahres auswählen";
             // 
@@ -139,7 +139,7 @@
             // button5
             // 
             this.button5.Enabled = false;
-            this.button5.Location = new System.Drawing.Point(748, 150);
+            this.button5.Location = new System.Drawing.Point(248, 150);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(147, 23);
             this.button5.TabIndex = 12;
@@ -149,7 +149,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(595, 150);
+            this.button6.Location = new System.Drawing.Point(95, 150);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(147, 23);
             this.button6.TabIndex = 13;
@@ -161,7 +161,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(907, 196);
+            this.ClientSize = new System.Drawing.Size(409, 196);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.textBox4);
--- Tagplaner/Tagplaner/View/FerienFeiertageAuswaehlenForm.cs
+++ Tagplaner/Tagplaner/View/FerienFeiertageAuswaehlenForm.cs
@@ -12,10 +12,10 @@ namespace Tagplaner.View
 {
     public partial class FerienFeiertageAuswaehlenForm : Form
     {
-        Boolean file1Choosen;/* = false;*/
-        Boolean file2Choosen;/* = false;*/
-        Boolean file3Choosen;/* = false;*/
-        Boolean file4Choosen;/* = false;*/
+        Boolean file1Choosen;
+        Boolean file2Choosen;
+        Boolean file3Choosen;
+        Boolean file4Choosen;
 
         String textFromTextBox1;
         String textFromTextBox2;
@@ -45,8 +45,6 @@ namespace Tagplaner.View
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //openHolidayFile(textBox1, file1Choosen, textFromTextBox1, "Feiertagdatei aktuelles Jahr öffnen");
-            
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
             System.IO.Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "Feiertage");
@@ -60,18 +58,15 @@ namespace Tagplaner.View
 
             if (fileChoiceResult == DialogResult.OK)
             {
-                this.textBox1.Text = openFileDialog1.FileName;
+                this.textBox1.Text = splitUrl(openFileDialog1.FileName);
                 this.textFromTextBox1 = openFileDialog1.FileName;
                 file1Choosen = true;
                 CheckAllFilesChoosen();
             }
-             
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //openHolidayFile(textBox2, file2Choosen, textFromTextBox2, "Feiertagdatei nächstes Jahr öffnen");
-
             OpenFileDialog openFileDialog2 = new OpenFileDialog();
 
             System.IO.Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "Feiertage");
@@ -85,18 +80,15 @@ namespace Tagplaner.View
 
             if (fileChoiceResult == DialogResult.OK)
             {
-                this.textBox2.Text = openFileDialog2.FileName;
+                this.textBox2.Text = splitUrl(openFileDialog2.FileName);
                 this.textFromTextBox2 = openFileDialog2.FileName;
                 file2Choosen = true;
                 CheckAllFilesChoosen();
             }
-            
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            //openVacationFile(textBox3, this.file3Choosen, textFromTextBox3, "Feriendatei aktuelles Jahr öffnen");
-            
             OpenFileDialog openFileDialog3 = new OpenFileDialog();
 
             System.IO.Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "Ferien");
@@ -110,18 +102,15 @@ namespace Tagplaner.View
 
             if (fileChoiceResult == DialogResult.OK)
             {
-                this.textBox3.Text = openFileDialog3.FileName;
+                this.textBox3.Text = splitUrl(openFileDialog3.FileName);
                 this.textFromTextBox3 = openFileDialog3.FileName;
                 file3Choosen = true;
                 CheckAllFilesChoosen();
             }
-             
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            //openVacationFile(textBox4, this.file4Choosen, textFromTextBox4, "Feriendatei nächstes Jahr öffnen");
-            
             OpenFileDialog openFileDialog4 = new OpenFileDialog();
 
             System.IO.Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "Ferien");
@@ -135,12 +124,11 @@ namespace Tagplaner.View
 
             if (fileChoiceResult == DialogResult.OK)
             {
-                this.textBox4.Text = openFileDialog4.FileName;
+                this.textBox4.Text = splitUrl(openFileDialog4.FileName);
                 this.textFromTextBox4 = openFileDialog4.FileName;
                 file4Choosen = true;
                 CheckAllFilesChoosen();
             }
-             
         }
 
         //OK-Button enablen wenn alles nötigen Dateien ausgewählt wurden
@@ -164,48 +152,17 @@ namespace Tagplaner.View
             this.Dispose(true);
         }
 
-        private void openVacationFile(TextBox textBox, bool fileChoosen, String textFromTextBox, String fileDialogTitle)
+        public String splitUrl(String url)
         {
-            OpenFileDialog openFileDialog3 = new OpenFileDialog();
-
-            System.IO.Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "Ferien");
-
-            openFileDialog3.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory + "Ferien";
-            openFileDialog3.Title = fileDialogTitle;
-            openFileDialog3.Filter = "Ferien|*.ics";
-            openFileDialog3.Multiselect = false;
-
-            DialogResult fileChoiceResult = openFileDialog3.ShowDialog();
-
-            if (fileChoiceResult == DialogResult.OK)
+            String splittedUrl;
+            int counter = 0;
+            String[] substrings = url.Split('\\');
+            foreach (String partOfSubstring in substrings)
             {
-                textBox.Text = openFileDialog3.FileName;
-                textFromTextBox = openFileDialog3.FileName;
-                fileChoosen = true;
-                CheckAllFilesChoosen();
+                counter++;
             }
-        }
-
-        private void openHolidayFile(TextBox textBox, Boolean fileChoosen, String textFromTextBox, String fileDialogTitle)
-        {
-            OpenFileDialog openFileDialog2 = new OpenFileDialog();
-
-            System.IO.Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "Feiertage");
-
-            openFileDialog2.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory + "Feiertage";
-            openFileDialog2.Title = fileDialogTitle;
-            openFileDialog2.Filter = "Feiertage|*.csv";
-            openFileDialog2.Multiselect = false;
-
-            DialogResult fileChoiceResult = openFileDialog2.ShowDialog();
-
-            if (fileChoiceResult == DialogResult.OK)
-            {
-                textBox.Text = openFileDialog2.FileName;
-                textFromTextBox = openFileDialog2.FileName;
-                fileChoosen = true;
-                CheckAllFilesChoosen();
-            }
+            splittedUrl = substrings[counter - 1];
+            return splittedUrl;
         }
     }
 }
