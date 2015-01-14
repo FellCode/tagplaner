e618f18eca6af597dc2099c89ded8217d74c4bb3
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-14 10:13:36 +0000

    - ExportPdf Button mit Logik gefüllt

--- Tagplaner/Tagplaner/View/ExportPdfForm.cs
+++ Tagplaner/Tagplaner/View/ExportPdfForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,34 @@ namespace Tagplaner
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            exportPdf();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void exportPdf()
+        {
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                List<MTrainer> trainerList = new List<MTrainer>();
+                trainerList.Add(new MTrainer("Alexander", "Theis", "AT", true, true));
+
+                CPdfExporter pdfExporter = new CPdfExporter(MCalendar.getInstance(), trainerList);
+                pdfExporter.ExportPdf(saveFileDialog1.FileName);
+            }
+
+            if (checkBox1.Checked && File.Exists(saveFileDialog1.FileName))
+            {
+                this.Close();
+                System.Diagnostics.Process.Start(saveFileDialog1.FileName);
+            }
+
+            this.Close();
         }
     }
 }
