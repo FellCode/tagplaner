beacd2d42eaee5500107a1f5caa9ed08d2a5c57b
lf2freek <lf2freek@gmail.com>
2015-01-14 15:32:01 +0000

    Controller und Interface für das Bearbeitungspanel hinzugefügt

---
+++ Tagplaner/Tagplaner/Interface/IChagepanel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tagplaner
+{
+    interface IChangepanel
+    {
+
+
+        void ChangeCalendarEntry(MCalendarEntry calendarentry, ComboBox tagartb, ComboBox seminarb,
+            ComboBox trainerb, ComboBox cotrainerb, ComboBox ortb, ComboBox raumb, TextBox kommentarb);
+
+        void PasteEntry(ComboBox tagartb, ComboBox seminarb,
+            ComboBox trainerb, ComboBox cotrainerb, ComboBox ortb, ComboBox raumb, TextBox kommentarb);
+
+        bool FillDayType(ComboBox Tagart);
+
+        bool FillSeminar(ComboBox Seminar);
+        
+        bool FillTrainer(ComboBox Trainer);
+        
+        bool FillCoTrainer(ComboBox CoTrainer);
+
+        bool FillLocation(ComboBox Ort);
+
+        bool FillRoom(ComboBox Ort, ComboBox Raum);
+        void UpdateCalenderEntry(ComboBox Tagart, TextBox Kommentar);
+        void ChangeVisibility(ComboBox Tagart, Panel Seminarpanel);
+
+    }
+}
