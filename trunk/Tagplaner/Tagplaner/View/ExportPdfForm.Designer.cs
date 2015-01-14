611080f49d423f3373cb464b6a2b464ad567494b
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-14 09:48:26 +0000

    - Export Button hinzugefügt

--- trunk/Tagplaner/Tagplaner/View/ExportPdfForm.Designer.cs
+++ trunk/Tagplaner/Tagplaner/View/ExportPdfForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Tagplaner.View
+﻿namespace Tagplaner
 {
     partial class ExportPdfForm
     {
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Export";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // ExportPdfForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Name = "ExportPdfForm";
             this.Text = "ExportPdfForm";
             this.Load += new System.EventHandler(this.ExportPdfForm_Load);
@@ -43,5 +55,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
--- trunk/Tagplaner/Tagplaner/View/ExportPdfForm.cs
+++ trunk/Tagplaner/Tagplaner/View/ExportPdfForm.cs
@@ -21,5 +21,10 @@ namespace Tagplaner
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
