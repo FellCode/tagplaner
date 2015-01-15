f824ee113737d2f6759908ed1ac422c3e603d077
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-15 09:54:13 +0000

    - Ölbohrturm als SplashScreen Image und interne Ressource hinzugefügt

--- trunk/Tagplaner/Tagplaner/View/SplashScreenForm.Designer.cs
+++ trunk/Tagplaner/Tagplaner/View/SplashScreenForm.Designer.cs
@@ -29,35 +29,55 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 222);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 435);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(489, 23);
-            this.button1.TabIndex = 0;
+            this.button1.Size = new System.Drawing.Size(587, 23);
+            this.button1.TabIndex = 2;
             this.button1.Text = "Überspringen nur zum Testen";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // SplashScreenFormcs
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Tagplaner.Properties.Resources.splashScreen;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(585, 440);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SplashScreenForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(513, 257);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(587, 458);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "SplashScreenFormcs";
+            this.Name = "SplashScreenForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "SplashScreenFormcs";
+            this.Text = "Tagplaner";
+            this.Load += new System.EventHandler(this.SplashScreenForm_Load);
             this.Shown += new System.EventHandler(this.SplashScreenFormcs_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
     }
 }
