36cd5621eb8ecde2ac617f6d1dee8b36aba950a5
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-15 08:45:57 +0000

    - Namespace angepasst - Logik eingefügt

--- Tagplaner/Tagplaner/View/SplashScreenFormcs.cs
+++ Tagplaner/Tagplaner/View/SplashScreenFormcs.cs
@@ -8,13 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Tagplaner.View
+namespace Tagplaner
 {
     public partial class SplashScreenFormcs : Form
     {
+        private Timer timer;
+
         public SplashScreenFormcs()
         {
             InitializeComponent();
+        }
+
+        private void SplashScreenFormcs_Shown(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Interval = 3000;
+            timer.Start();
+            timer.Tick += timer_tick;
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            FormInit formInit = new FormInit();
+            formInit.Show();
+            this.Hide();
+        }
+
+        private void SplashScreenFormcs_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit(); 
         }
     }
 }
