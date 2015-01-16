a473ffe11a7f3a3d76afed6ba12a64662849cf54
f.smudo <F.Smudo@gmail.com>
2015-01-16 12:37:19 +0000

    Kommentare

--- Tagplaner/Tagplaner/View/TagplanAnlegenUserControl.cs
+++ Tagplaner/Tagplaner/View/TagplanAnlegenUserControl.cs
@@ -11,7 +11,7 @@ using System.IO;
 
 namespace Tagplaner
 {
-   
+ 
     public partial class TagplanAnlegenUserControl : UserControl
     {
         Tagplaner.View.FerienFeiertageAuswaehlenForm ferienFeiertageAuswaehlenForm;
