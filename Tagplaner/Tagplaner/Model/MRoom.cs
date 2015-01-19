e394b7fff0206b3d6c2a887bcd6e23769632730f
i.pfeuster <i.pfeuster@gmail.com>
2015-01-19 10:17:37 +0000

    Kommentare

--- Tagplaner/Tagplaner/Model/MRoom.cs
+++ Tagplaner/Tagplaner/Model/MRoom.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Tagplaner
 {
+    /// <summary>
+    /// Klasse für das Model für den Raum
+    /// </summary>
     [Serializable()]
     public class MRoom
     {
@@ -29,19 +32,33 @@ namespace Tagplaner
         #endregion
 
         #region constructor
-
+        /// <summary>
+        /// Konstruktor mit Raumnummer
+        /// </summary>
+        /// <param name="number">Raumnummer</param>
         public MRoom( string number)
         {
          
             this.number = number;
         }
 
+        /// <summary>
+        /// Konstruktor mit Id und Raumnummer
+        /// </summary>
+        /// <param name="id">Id</param>
+        /// <param name="number">Raumnummer</param>
         public MRoom(int id, string number)
         {
             this.id = id;
             this.number = number;
         }
 
+        /// <summary>
+        /// Konstruktor mit Id, Raumnummer und StandortID
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <param name="number">Raumnummer</param>
+        /// <param name="place_id">Id des Standorts des Tagplanes</param>
         public MRoom(int id, string number, int place_id)
         {
             this.id = id;
@@ -49,6 +66,11 @@ namespace Tagplaner
             this.place_id = place_id;
         }
 
+        /// <summary>
+        /// Konstruktor mit  Raumnummer und StandortID
+        /// </summary>
+        /// <param name="number">Raumnummer</param>
+        /// <param name="place_id">Id des Standorts des Tagplanes</param>
         public MRoom( string number, int place_id)
         {
             this.number = number;
@@ -56,6 +78,11 @@ namespace Tagplaner
         }
         #endregion
 
+        /// <summary>
+        /// Überschreibt die ToString Methode zur Darstellung der Raumnummer in den ComboBoxen auf
+        /// der grafischen Oberfläche
+        /// </summary>
+        /// <returns>Raumnummer</returns>
         public override string ToString()
         {
             return number;
