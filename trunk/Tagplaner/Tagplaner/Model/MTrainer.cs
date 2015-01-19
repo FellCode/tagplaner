b52c83fb4ce41b2f3b548f62ee5e691ca3d2324d
i.pfeuster@gmail.com <i.pfeuster@gmail.com@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-19 09:51:55 +0000

    Kommentare

--- trunk/Tagplaner/Tagplaner/Model/MTrainer.cs
+++ trunk/Tagplaner/Tagplaner/Model/MTrainer.cs
@@ -50,6 +50,14 @@ namespace Tagplaner
         #endregion
 
         #region constructor
+        /// <summary>
+        /// Konstruktor mit Nachname, Vorname, Kürzel und istIntern und istCotrainer
+        /// </summary>
+        /// <param name="name">Nachname des Trainers</param>
+        /// <param name="surname">Vorname des Trainers</param>
+        /// <param name="abbreviation">Kürzel des Trainers</param>
+        /// <param name="isInternal">Flag ob Intern oder Extern</param>
+        /// <param name="isCotrainer">Falg ob Cotrainer oder nicht</param>
         public MTrainer( string name, string surname, string abbreviation, bool isInternal, bool isCotrainer)
         {
             this.name = name;
@@ -58,7 +66,15 @@ namespace Tagplaner
             this.isInternal = isInternal;
             this.isCotrainer = isCotrainer;
         }
-
+        /// <summary>
+        /// Konstruktor mit Id, Nachname, Vorname, Kürzel und istIntern
+        /// </summary>
+        /// <param name="id">Datenbank ID</param>
+        /// <param name="name">Nachname des Trainers</param>
+        /// <param name="surname">Vorname des Trainers</param>
+        /// <param name="abbreviation">Kürzel des Trainers</param>
+        /// <param name="isInternal">Flag ob intern oder extern</param>
+        /// </summary>
         public MTrainer(int id, string name, string surname, string abbreviation, bool isInternal)
         {
             this.id = id;
@@ -68,7 +84,13 @@ namespace Tagplaner
             this.isInternal = isInternal;
             
         }
-
+        /// <summary>
+        /// Konstruktor mit Nachname, Vorname, Kürzel und istIntern
+        /// </summary>
+        /// <param name="name">Nachname des Trainers</param>
+        /// <param name="surname">Vorname des Trainers</param>
+        /// <param name="abbreviation">Kürzel des Trainers</param>
+        /// <param name="isInternal">Flag ob intern oder extern</param>
         public MTrainer(string name, string surname, string abbreviation, bool isInternal)
         {
          
