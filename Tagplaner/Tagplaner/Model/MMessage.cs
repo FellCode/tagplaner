69d89277ac5453aeab9652538eb58bd599a7f853
javaseminar2014 <javaseminar2014@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-20 09:27:04 +0000

    - Nachrichten hinzugefügt

--- Tagplaner/Tagplaner/Model/MMessage.cs
+++ Tagplaner/Tagplaner/Model/MMessage.cs
@@ -13,11 +13,19 @@ namespace Tagplaner
     public class MMessage
     {
         private MMessage() { }
-        public static string CONFIRMATION_EXAMPLE = "Beispiel für eine Bestätigung";
-        public static string WARNING_EXAMPLE = "Beispiel für eine Warnung";
-        public static string INFORMATION_EXAMPLE = "Beispiel für eine Information";
-        public static string ERROR_EXAMPLE = "Beispiel für eine Fehlermeldung";
-
+        /// <summary>
+        /// Bestätigungsabfrage: Möchten Sie die aktuellen Änderungen speichern?
+        /// </summary>
         public static string CONFIRMATION_FILE_SAVE = "Möchten Sie die aktuellen Änderungen speichern?";
+        
+        /// <summary>
+        /// Warnung: Datei konnte nicht geöffnet werden!
+        /// </summary>
+        public static string WARINING_FILE_NOT_FOUND = "Datei konnte nicht geöffnet werden!";
+
+        /// <summary>
+        /// Fehler: Datei  enthält keine Einträge zwischen Start- oder Enddatum!
+        /// </summary>
+        public static string ERROR_NO_ENTRIES_BETWEEN_DATES = "Datei  enthält keine Einträge zwischen Start- oder Enddatum!";
     }
 }
