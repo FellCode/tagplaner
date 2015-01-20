59413990239c80109513c30db217805e541592a3
max.thill <max.thill@8682a707-2788-0eb7-e4f9-8d89aac99fbc>
2015-01-20 14:06:47 +0000

    -Messages hinzugefügt

--- trunk/Tagplaner/Tagplaner/Model/MMessage.cs
+++ trunk/Tagplaner/Tagplaner/Model/MMessage.cs
@@ -24,6 +24,21 @@ namespace Tagplaner
         public static string WARINING_FILE_NOT_FOUND = "Datei konnte nicht geöffnet werden!";
 
         /// <summary>
+        /// Warnung: Keine Klasse in Jahrgang gewählt!
+        /// </summary>
+        public static string WARINING_NO_CLASSES_CHOOSEN = "Keine Klasse in Jahrgang gewählt!";
+
+        /// <summary>
+        /// Warnung: Keine Klassenbezeichnung angegeben!
+        /// </summary>
+        public static string WARINING_NO_IDENTIFICATION_SET = "Keine Klassenbezeichnung angegeben!";
+
+        /// <summary>
+        /// Fehler: Keine Klasse in Jahrgang gewählt!
+        /// </summary>
+        public static string ERROR_STARTDATE_BIGGER_ENDDATE = "Das Anfangsdatum liegt vor dem Enddatum!";
+
+        /// <summary>
         /// Fehler: Datei  enthält keine Einträge zwischen Start- oder Enddatum!
         /// </summary>
         public static string ERROR_NO_ENTRIES_BETWEEN_DATES = "Datei  enthält keine Einträge zwischen Start- oder Enddatum!";
